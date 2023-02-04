d9773b619c78d365da8eb5677017c699bd4d04ba
WKWKA <ctd0606@naver.com>
2023-02-05 03:03:31 +0900

    about

--- src/Data.js
+++ src/Data.js
@@ -1,5 +1,34 @@
-
+import {AiOutlineUser, AiTwotonePhone, AiOutlineMail} from 'react-icons/ai'
+import {FaBirthdayCake } from 'react-icons/fa' 
 
 
 
 export const navLinks = ["home","about","skills","portfolio","contant"]
+
+export const introductions = [
+    {
+        id:1,
+        icon:<AiOutlineUser/>,
+        key:"Name",
+        text:"최석윤"
+        
+    },
+    {
+        id:2,
+        icon:<FaBirthdayCake/>,
+        key:"Birth",
+        text:"1995.03.16"
+    },
+    {
+        id:3,
+        icon:<AiTwotonePhone/>,
+        key:"Phone",
+        text:"010-7469-6008"
+    },
+    {
+        id:4,
+        icon:<AiOutlineMail/>,
+        key:"Email",
+        text:"ctd0606@naver.com"
+    },
+]
--- src/components/container/about/About.jsx
+++ src/components/container/about/About.jsx
@@ -1,10 +1,37 @@
-
+import portfolioImg from '../../../assets/201812101719791976_2.jpg'
+import { introductions }from '../../../Data'
 import './About.scss'
 
 export default function About() {
   return (
     <div className='container' id='about'>
-      About
+      <div className='title'>
+        <h1>About Me</h1>
+      </div>
+      <div className='about_container'>
+        <div className='about_left'>
+          <img src={portfolioImg} alt="portfolioImg" />
+        </div>
+        <div className='about_right'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis minus nisi fugit est dicta cum mollitia. Sed 
+            eaque laboriosam officiis laborum similique aperiam hic ut! 
+            Necessitatibus maxime natus laudantium ipsa.
+          </p>
+        
+            <div className='introduction'>
+            {introductions.map(introduction =>(
+              <div className='introduction_info' key={introduction.id}>
+                <span className='introduction_key'>{introduction.icon}{introduction.key}</span>
+                <span className='introduction_text'>{introduction.text}</span>
+              </div>
+              ))}
+            </div>
+            
+         
+        </div>
+      </div>
     </div>
   )
 }
